49a395018316fd8faa6a9c36c4ecff0d2143c537
Garlyle <kovacsmilan85@gmail.com>
2025-04-11 16:27:58 +0200

    organize code

--- src/App.py
+++ src/App.py
@@ -2,30 +2,38 @@ import pygame
 
 class App:
     def __init__(self, w, h, caption="Title", flags = 0):
+        """Initialize PyGame window"""
         pygame.init()
         self.info = pygame.display.Info()
         self.screen = pygame.display.set_mode([w, h], flags)
         pygame.display.set_caption(caption)
 
     def set_window(self, w, h):
+        """Change Window resolution"""
         self.screen = pygame.display.set_mode([w, h])
     
     def set_fullscreen(self):
+        """Set window to full screen"""
         self.screen = pygame.display.set_mode((self.info.current_w, self.info.current_h), flags=pygame.SCALED | pygame.FULLSCREEN)
 
     def clear(self, color = (0, 0, 0)):
+        """Clear the screen to color (default: black)"""
         self.screen.fill(color)
 
     def draw(self, sprite: pygame.sprite.Sprite, dest = None, area = None):
+        """Draw a Sprite to screen"""
         self.screen.blit(source = sprite.image, 
             dest = dest,
             area = area)
 
     def render(self):
+        """Render display surface to screen"""
         pygame.display.flip()
     
     def make_screenshot(self, filename="screenshot.png"):
+        """save a screenshot"""
         pygame.image.save(self.screen, filename)
 
     def close(self):
+        """exit the application"""
         pygame.quit()
--- test.py
+++ test.py
@@ -1,14 +1,24 @@
 import pygame
 from src.App import App
 
-app = App(640, 480)
+# Constants
+FPS = 60
+WIDTH = 640
+HEIGHT = 480
+
+# Pygame Essentials
+app = App(WIDTH, HEIGHT)
 clock = pygame.time.Clock()
 running = True
 delta = 0
+
+# Variables
 rgb = [0, 128, 128, 255]
 index = 0
 
+# Main Loop
 while running:
+    # Handle Events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -20,14 +30,11 @@ while running:
             if event.key == pygame.K_d:
                 index = index-1 if (index - 1 >= 0) else 2
             if event.key == pygame.K_1:
-                app.set_window(640, 480)
+                app.set_window(WIDTH, HEIGHT)
             if event.key == pygame.K_2:
                 app.set_fullscreen()
             if event.key == pygame.K_F12:
                 app.make_screenshot()
-
-    # fill the screen with a color to wipe away anything from last frame
-    app.clear(rgb)
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
@@ -35,9 +42,14 @@ while running:
     if keys[pygame.K_s]:
         rgb[index] -= 1
     
+    # Update
     rgb[index] = max(0, min(rgb[index], 255))
 
+    # Render
+    app.clear(rgb)
     app.render()
-    delta = clock.tick(60) / 1000
 
+    delta = clock.tick(FPS) / 1000
+
+# Clean Up
 app.close()
